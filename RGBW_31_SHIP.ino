25340ada574b523640dd2fa1543afc78a3218134
spen0007 <spen0007@users.noreply.github.com>
2019-04-29 10:00:15 +0100

    Update RGBW_31_SHIP.ino

--- RGBW_31_SHIP.ino
+++ RGBW_31_SHIP.ino
@@ -47,7 +47,7 @@
  * RGBW LED Controller v 3.1IR
  * Jeremy Saglimbeni - 2013
  * 
- * Added binary mode
+ * Added a new binary mode communication protocol for efficiency
  * divided blue output by 2, as the resistor is different on the blue
  * channel and it is therefore brighter per bit
  * spen0007 - 2016
